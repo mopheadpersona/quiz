89f846a2523cd963e4853a9c463848af8b038d3b
Dmitrii Nazarenko <deemaneken@gmail.com>
2021-02-20 18:31:09 +0100

    add create quiz

--- application.py
+++ application.py
@@ -1,10 +1,23 @@
 from flask import Flask, abort, render_template, request
+#from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy.dialects.postgresql import JSON
+
 from data import test_quiz, test_humans, test_quizes
 
+
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://deemaneken:star@localhost/quiz_db'
+#db = SQLAlchemy(app)
+#
+#class Quiz(db.Model):
+#	id = db.Column(db.Integer, primary_key=True)
+#	json_column = db.Column(JSON)
+#
+#db.create_all()
+
 
 @app.route('/')
-def index():
+def index(): 
 	return render_template('home.html', quizes=test_quizes)
 
 @app.route('/quiz')
@@ -12,11 +25,43 @@ def quiz():
 	# return test_quiz
 	return render_template('quiz.html', quiz=test_quiz)
 
+@app.route('/create-quiz')
+def add_quiz():
+	return render_template('create.html')
+
+@app.route('/submit-new-quiz', methods=['POST'])
+def create_quiz():
+	data = {}
+	query = request.form
+	counter = 0
+
+	for key in query:
+		if key.startswith('questions[question]'):
+			counter += 1
+
+	questions = []
+	for i in range(1, counter + 1):
+		question = {}
+		question['question'] = query[f'questions[question][{i}]']
+		answers = []
+		for j in range(1, 5):
+			answer = {}
+			answer['answer'] = query[f'answers[question][{i}][answer][{j}]']
+			answers.append(answer)
+		question['answers'] = answers
+		questions.append(question)
+
+	data['name'] = query['name']
+	data['description'] = query['description']
+	data['length'] = str(counter)
+	data['questions'] = questions
+
+	return data
+
 @app.route('/submit-quiz', methods=['POST'])
 def process_quiz():
 	results = request.form
 	points = 0
-
 
 	for question in test_quiz['questions']:
 		if results[question['id']] == question['correct_answer_id']:
